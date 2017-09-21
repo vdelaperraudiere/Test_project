c6f3126bae3ca7de3554b9c503cb42ab3b7f9a0e
Valentin DE LA PERRAUDIERE <valentin.de-la-perraudiere@nextflow-software.com>
2017-09-21 16:54:40 +0200

    changed hello again

--- hello.c
+++ hello.c
@@ -4,5 +4,11 @@
 
 void sayHello()
 {
-	printf("Hello\n");
+	i = 0; /* initialisation de i à la valeur 0 */
+	while (i<1000) /* exécution tant que la valeur i est inférieure à 1000 */
+	{
+		printf("Hello\n");
+		i++;  /* incrémentation de la variable i (i++ est une écriture condensée d'incrémentation de la variable i, équivalente à i = i + 1, la dernire forme étant à préférer, pour des raisons de lisibilité */
+	}
+	
 }
